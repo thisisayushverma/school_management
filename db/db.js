c52d2d91ca82927c25778d97f302bb973b3af28c
Ayush Verma <devilofilluminati@gmail.com>
2024-12-04 10:16:34 +0530

    finally project completed

--- db/db.js
+++ db/db.js
@@ -1,12 +1,18 @@
 import mysql from "mysql"
+import fs from "fs"
+
+
 
 const con = mysql.createConnection({
-  host:process.env.HOST,
-  port:process.env.PORT,
-  user:process.env.USERNAME,
-  password:process.env.PASSWORD,
-  database:process.env.DATABASE,
-  connectTimeout: 10000
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 4000,
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'test',
+  ssl: process.env.TIDB_ENABLE_SSL === 'true' ? {
+      minVersion: 'TLSv1.2',
+      ca: process.env.TIDB_CA_PATH ? fs.readFileSync(process.env.TIDB_CA_PATH) : undefined
+  } : null,
 })
 
 
